447d9af9b03dc571e88d8787e95ed983cd058363
Capizzi Filippo <capizzi@rsaweb.com>
2023-10-18 16:35:40 +0200

    Aggiunto controller e modello per pratiche

---
+++ MedieticaWebApiService/Controller/PraticheController.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using MedieticaWebApiService.Extensions;
+using MedieticaWebApiService.Helpers;
+using MedieticaWebApiService.Models;
+
+namespace MedieticaWebApiService.Controller
+{
+	[EnableCors("*", "*", "*")]
+
+	public class PraticheController : ApiController
+	{
+		[HttpGet]
+		[Route("api/pratiche/blank")]
+		[Route("api/pratiche/blank/{ditta}")]
+		public DefaultJson<PraticheDb> Blank(int ditta = 0)
+		{
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					var json = new DefaultJson<PraticheDb>();
+
+					cmd.CommandText = DbUtils.QueryAdapt("SELECT COALESCE(MAX(pra_codice),0) AS codice FROM pratiche");
+					var reader = cmd.ExecuteReader();
+					while (reader.Read())
+					{
+						var pra = new PraticheDb();
+						pra.pra_codice = 1 + reader.GetInt64(reader.GetOrdinal("codice"));
+						if (json.Data == null) json.Data = new List<PraticheDb>();
+						json.Data.Add(pra);
+						json.RecordsTotal++;
+					}
+					reader.Close();
+					connection.Close();
+
+					return (json);
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+
+		[HttpGet]
+		[Route("api/pratiche/get")]
+		public DefaultJson<PraticheDb> GetList(int top = 0, int skip = 0, string orderby = "", string search = "", string filter = "", bool inlinecount = false, bool joined = false)
+		{
+			if (filter.SqlDangerCheck()) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Danger filter value"));
+			if (search.SqlDangerCheck()) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Danger search value"));
+			if (orderby.SqlDangerCheck()) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Danger orderby value"));
+
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					var json = new DefaultJson<PraticheDb>();
+
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					DbUtils.CheckAuthorization(cmd, Request, 0, Endpoints.DITTE, EndpointsOperations.VIEW);
+
+					var str = search.ToUpper().SqlQuote(true, true);
+					string query;
+					var total = 0L;
+					if (inlinecount)
+					{
+						if (joined)
+							query = PraticheDb.GetCountQuery();
+						else
+							query = "SELECT COUNT(*) FROM pratiche";
+						if (string.IsNullOrWhiteSpace(filter))
+							query += " WHERE pra_codice > 0";
+						else
+							query += $" WHERE pra_codice > 0 AND ({filter})";
+
+						if (!string.IsNullOrWhiteSpace(search))
+						{
+							if (joined)
+								query += $" AND (TRIM(CAST(pra_codice AS VARCHAR(15))) ILIKE {str})";
+							else
+								query += $" AND (TRIM(CAST(pra_codice AS VARCHAR(15))) ILIKE {str})";
+						}
+
+						cmd.CommandText = DbUtils.QueryAdapt(query);
+						total = (long)cmd.ExecuteScalar();
+					}
+
+					if (joined)
+						query = PraticheDb.GetJoinQuery();
+					else
+						query = "SELECT * FROM pratiche";
+						
+					if (string.IsNullOrWhiteSpace(filter))
+						query += " WHERE pra_codice > 0";
+					else 
+						query += " WHERE pra_codice > 0 AND (" + filter + ")";
+
+					if (!string.IsNullOrWhiteSpace(search))
+					{
+						if (joined)
+							query += $" AND (TRIM(CAST(pra_codice AS VARCHAR(15))) ILIKE {str})";
+						else
+							query += $" AND (TRIM(CAST(pra_codice AS VARCHAR(15))) ILIKE {str})";
+					}
+					if (string.IsNullOrWhiteSpace(orderby))
+						query += " ORDER BY pra_codice";
+					else
+						query += " ORDER BY " + orderby;
+					cmd.CommandText = DbUtils.QueryAdapt(query, top, skip);
+
+					var reader = cmd.ExecuteReader();
+					while (reader.Read())
+					{
+						var pra = new PraticheDb();
+						DbUtils.SqlRead(ref reader, ref pra, joined ? null : PraticheDb.GetJoinExcludeFields());
+						if (json.Data == null) json.Data = new List<PraticheDb>();
+						json.Data.Add(pra);
+						json.RecordsTotal++;
+					}
+					reader.Close();
+					connection.Close();
+
+					if (inlinecount) json.RecordsTotal = total;
+					return (json);
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (HttpResponseException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+		[HttpGet]
+		[Route("api/pratiche/get/{codice}")]
+		[Route("api/pratiche/get/{codice}/{joined}")]
+		[Route("api/pratiche/get/{codice}/{joined}/{fulljoined}")]
+		public DefaultJson<PraticheDb> Get(int codice, bool joined = false, bool fulljoined = false)
+		{
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					DbUtils.CheckAuthorization(cmd, Request, 0, Endpoints.DITTE, EndpointsOperations.VIEW);
+
+					var json = new DefaultJson<PraticheDb>();
+					var pra = new PraticheDb();
+					if (PraticheDb.Search(ref cmd,  codice, ref pra, joined || fulljoined))
+					{
+						if (json.Data == null) json.Data = new List<PraticheDb>();
+						json.Data.Add(pra);
+						json.RecordsTotal++;
+					}
+
+					connection.Close();
+					return (json);
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (HttpResponseException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+		[HttpPost]
+		[Route("api/pratiche/post")]
+		public DefaultJson<PraticheDb> Post([FromBody] DefaultJson<PraticheDb> value)
+		{
+			if (value == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Null input value"));
+			if (value.Data == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Null Data value"));
+			if (value.Data.Count != value.RecordsTotal) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Il numero di record non corrispondente"));
+			
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					DbUtils.CheckAuthorization(cmd, Request, 0, Endpoints.DITTE, EndpointsOperations.ADD);
+					var codute = DbUtils.GetTokenUser(Request);
+
+					var json = new DefaultJson<PraticheDb>();
+					foreach (var pra in value.Data)
+					{
+						object obj = null;
+						var val = pra;
+						val.pra_user = codute;
+
+						DbUtils.SqlWrite(ref cmd, PraticheDb.Write, DbMessage.DB_INSERT, ref val, ref obj, true);
+
+						if (json.Data == null) json.Data = new List<PraticheDb>();
+						json.Data.Add(val);
+						json.RecordsTotal++;
+					}
+					connection.Close();
+					return (json);
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (HttpResponseException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+		[HttpPut]
+		[Route("api/pratiche/put/{codice}")]
+		public DefaultJson<PraticheDb> Put(int codice, [FromBody]DefaultJson<PraticheDb> value)
+		{
+			if (value == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Null input value"));
+			if (value.Data == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Null Data value"));
+			if (value.Data.Count != value.RecordsTotal) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Il numero di record non corrispondente"));
+			if (value.Data.Count != 1) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Il numero di record non valido"));
+
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					DbUtils.CheckAuthorization(cmd, Request, 0, Endpoints.DITTE, EndpointsOperations.UPDATE);
+
+					var pra = value.Data[0];
+					if (pra.pra_codice != codice) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest, "Id risorsa non corrisponde all'id dei dati"));
+
+					object obj = null;
+					
+					var json = new DefaultJson<PraticheDb>();
+					pra.pra_user = DbUtils.GetTokenUser(Request);
+					DbUtils.SqlWrite(ref cmd, PraticheDb.Write, DbMessage.DB_UPDATE, ref pra, ref obj, true);
+
+					connection.Close();
+					if (json.Data == null) json.Data = new List<PraticheDb>();
+					json.Data.Add(pra);
+					json.RecordsTotal++;
+
+					return (json);
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (HttpResponseException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+		[HttpDelete]
+		[Route("api/pratiche/delete/{codice}")]
+		public void Delete(int codice)
+		{
+			try
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var cmd = new OdbcCommand { Connection = connection };
+					DbUtils.CheckAuthorization(cmd, Request, 0, Endpoints.DITTE, EndpointsOperations.DELETE);
+
+					var val = new PraticheDb();
+					if (!PraticheDb.Search(ref cmd, codice, ref val)) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound, "Risorsa non trovata"));
+
+					object obj = null;
+					DbUtils.SqlWrite(ref cmd, PraticheDb.Write, DbMessage.DB_DELETE, ref val, ref obj);
+
+					connection.Close();
+				}
+			}
+			catch (MCException ex)
+			{
+				var res = new McResponse(ExceptionsType.MC_EXCEPTION, ex.GetError(), ex.Message, ex.GetStackTrace());
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (OdbcException ex)
+			{
+				var err = 0;
+				if (ex.Errors.Count > 0) err = ex.Errors[0].NativeError;
+				var res = new McResponse(ExceptionsType.ODBC_EXCEPTION, err, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+			catch (HttpResponseException)
+			{
+				throw;
+			}
+			catch (HttpRequestException)
+			{
+				throw;
+			}
+			catch (Exception ex)
+			{
+				var res = new McResponse(ExceptionsType.GENERIC_EXCEPTION, 0, ex.Message, ex.StackTrace);
+				throw new HttpResponseException(Request.CreateResponse<McResponse>(HttpStatusCode.InternalServerError, res));
+			}
+		}
+
+	}
+}
---
+++ MedieticaWebApiService/Models/PraticheDb.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using MedieticaWebApiService.Helpers;
+
+namespace MedieticaWebApiService.Models
+{
+	public class PraticheDb
+	{
+		public long pra_codice { get; set; }
+		public long pra_dit { get; set; }
+		public long pra_cli { get; set; }
+		public long pra_fin { get; set; }
+		public long pra_inc { get; set; }
+		public long pra_ges { get; set; }
+		public long pra_cmr { get; set; }
+		public long pra_set { get; set; }
+		public long pra_str { get; set; }
+		public DateTime? pra_data { get; set; }
+		public DateTime? pra_data_inc { get; set; }
+		public string pra_note { get; set; }
+		public DateTime? pra_created_at { get; set; }
+		public DateTime? pra_last_update { get; set; }
+		public int pra_user { get; set; }
+
+		//
+		// Joined fields
+		// 
+		public string dit_desc { get; set; }
+		public string cli_desc { get; set; }
+		public string cli_codfis { get; set; }
+		public string cli_piva { get; set; }
+		public string fin_desc { get; set; }
+		public string inc_desc { get; set; }
+		public string ges_desc { get; set; }
+		public string cmr_desc { get; set; }
+		public string set_desc { get; set; }
+		public string stru_desc { get; set; }
+
+
+		public PraticheDb()
+		{
+			var pra_db = this;
+			DbUtils.Initialize(ref pra_db);
+		}
+
+		private static readonly List<string> InsExcludeFields = new List<string>() { "pra_codice", "dit_desc", "cli_desc", "cli_codfis", "cli_piva", "fin_desc", "inc_desc", "ges_desc", "cmr_desc", "set_desc", "stru_desc" };
+		private static readonly List<string> ExcludeFields = new List<string>() { "dit_desc", "cli_desc", "cli_codfis", "cli_piva", "fin_desc", "inc_desc", "ges_desc", "cmr_desc", "set_desc", "stru_desc" };
+
+		private static readonly string JoinQuery = @"
+		SELECT pratiche.*
+		FROM pratiche
+		LEFT JOIN ditte ON pra_dit = dit_codice
+		LEFT JOIN clienti ON pra_cli = cli_codice
+		LEFT JOIN finalita ON pra_fin = fin_codice
+		LEFT JOIN incarichi ON pra_inc = inc_codice
+		LEFT JOIN gestori ON pra_ges = ges_codice
+		LEFT JOIN commerciali ON pra_cmr = cmr_codice
+		LEFT JOIN settori ON pra_set = set_codice
+		LEFT JOIN strumenti ON pra_str = str_codice
+		";
+
+		private static readonly string CountQuery = @"
+		SELECT COUNT(*) 
+		FROM pratiche";
+
+		public static string GetJoinQuery()
+		{
+			return (JoinQuery);
+		}
+
+		public static List<string> GetJoinExcludeFields()
+		{
+			return (ExcludeFields);
+		}
+
+		public static string GetCountQuery()
+		{
+			return (CountQuery);
+		}
+
+		public static bool Search(ref OdbcCommand cmd, long codice, ref PraticheDb pra, bool joined = false,  bool writeLock = false)
+		{
+			if (pra != null) DbUtils.Initialize(ref pra);
+			if (codice == 0) return (true);
+
+			if (cmd == null)
+			{
+				using (var connection = new OdbcConnection(DbUtils.GetConnectionString()))
+				{
+					connection.Open();
+					var command = new OdbcCommand { Connection = connection };
+					return Search(ref command, codice, ref pra, joined, writeLock);
+				}
+			}
+
+			var found = false;
+			string sql;
+			if (!joined)
+				sql = "SELECT * FROM pratiche WHERE pra_codice = ?";
+			else
+				sql = GetJoinQuery() + " WHERE pra_codice = ?";
+			if (writeLock && !joined) sql += " FOR UPDATE NOWAIT";
+			cmd.CommandText = sql;
+			cmd.Parameters.Clear();
+			cmd.Parameters.Add("codice", OdbcType.BigInt).Value = codice;
+			var reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+				if (pra != null) DbUtils.SqlRead(ref reader, ref pra, joined ? null : ExcludeFields );
+				found = true;
+			}
+			reader.Close();
+			return (found);
+		}
+
+		public static void Write(ref OdbcCommand cmd, DbMessage msg, ref PraticheDb pra, ref object obj, bool joined = false)
+		{
+			DbUtils.Trim(ref pra);
+			if (msg == DbMessage.DB_UPDATE || msg == DbMessage.DB_REWRITE || msg == DbMessage.DB_DELETE || msg == DbMessage.DB_CLEAR)
+			{
+				var old = new PraticheDb();
+				if (!Search(ref cmd, pra.pra_codice, ref old, false, true)) throw new MCException(MCException.DeletedMsg, MCException.DeletedErr);
+				if (old.pra_last_update != pra.pra_last_update) throw new MCException(MCException.ModifiedMsg, MCException.ModifiedErr);
+			}
+
+			if (msg == DbMessage.DB_INSERT || msg == DbMessage.DB_UPDATE || msg == DbMessage.DB_BULK_INS)
+			{
+			}
+
+			switch (msg)
+			{
+				case DbMessage.DB_INSERT:
+					do
+					{
+						try
+						{
+							cmd.CommandText = DbUtils.SqlCommand(ref cmd, DbMessage.DB_INSERT, ref pra, "pretiche", null, InsExcludeFields);
+							var reader = cmd.ExecuteReader();
+							while (reader.Read())
+							{
+								DbUtils.SqlRead(ref reader, ref pra, ExcludeFields);
+							}
+							reader.Close();
+							if (joined) Reload(ref cmd, ref pra, true);
+						}
+						catch (OdbcException ex)
+						{
+							if (DbUtils.IsDupKeyErr(ex))
+							{
+								pra.pra_codice++;
+								continue;
+							}
+							throw;
+						}
+						break;
+					} while (true);
+					break;
+
+				case DbMessage.DB_REWRITE:
+				case DbMessage.DB_UPDATE:
+					cmd.CommandText = DbUtils.SqlCommand(ref cmd, DbMessage.DB_UPDATE, ref pra, "pratiche", "WHERE pra_codice = ?", ExcludeFields);
+					cmd.Parameters.Add("@codice", OdbcType.BigInt).Value = pra.pra_codice;
+					cmd.ExecuteNonQuery();
+					Reload(ref cmd, ref pra, joined);
+					break;
+
+				case DbMessage.DB_CLEAR:
+				case DbMessage.DB_DELETE:
+					{
+						cmd.CommandText = DbUtils.QueryAdapt("DELETE FROM pratiche WHERE ges_codice = ?");
+						cmd.Parameters.Clear();
+						cmd.Parameters.Add("codice", OdbcType.BigInt).Value = pra.pra_codice;
+						cmd.ExecuteNonQuery();
+					}
+					break;
+
+			}
+		}
+
+		public static void Reload(ref OdbcCommand cmd, ref PraticheDb pra, bool joined)
+		{
+			if (!Search(ref cmd, pra.pra_codice, ref pra, joined))
+			{
+				throw new MCException(MCException.DeletedMsg, MCException.DeletedErr);
+			}
+		}
+
+	}
+}
